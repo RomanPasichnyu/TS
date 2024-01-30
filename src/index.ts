daee0c89e4163dac89f9b320646e3ab9221f4563
RomanPasichnyu <romjkea@gmail.com>
2024-01-30 22:00:04 +0200

    ts2

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,57 @@
-console.log('wdasad');
+ interface ITodo{
+    id:number;
+     title: string
+ }
+ class Notebook{
+    private _todos: Itodo[]
+     constructor(private name:string) {
+        this._initMain()
+    }
+    private _getTodosFromLS():void{
+        this._todos = JSON.parse(localStorage.getItem(this.name))|| []
+    }
+    private _setTodosLS():void{
+        localStorage.setItem(this.name, JSON.stringify(this._todos))
+        this._initTodos()
+
+    }
+
+
+    private _initMain():void{
+        this._initForm()
+        this._initTodos()
+
+
+    }
+
+    private _initTodos():void{
+        this._getTodosFromLS()
+        let todosDiv = document.querySelector('#todos') as HTMLDivElement;
+        todosDiv.innerHTML= ''
+        this._todos.forEach(todo=>{
+            let todoDiv = document.createElement('div');
+            todoDiv.innerText = `${todo.id}) ${todo.title}`
+            todosDiv.appendChild(todoDiv)
+
+
+        })
+    }
+
+    private _initForm():void{
+        let form = document.forms['form'] as HTMLFormElement;
+        form.onsubmit=(e)=>{
+            e.preventDefault();
+            let input = e.target['title'] as HTMLInputElement;
+            let id = this._todos.slice(-1)[0]?.id+1 || 1;
+            this._todos.push({id, title:input.value})
+            this._setTodosLS();
+            form.reset();
+
+        }
+    }
+
+
+
+ }
+
+ new Notebook('notebook3')
